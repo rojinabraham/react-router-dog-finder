a394d0b3a9453ca05b4041cec7f777176bba7a51
Rojin Abraham <rojinabraham@gmail.com>
2020-08-17 16:46:14 +0530

    fin

--- src/DogList.js
+++ src/DogList.js
@@ -5,12 +5,12 @@ class DogList extends Component {
   render() {
     return (
       <div className="DogList">
-        <h1 className="display-1 text-center">Dog List</h1>
+        <h1 className="display-1 text-center my-4">Dog List</h1>
         <div className="row">
           {this.props.dogs.map((d) => (
             <div className="Dog col-lg-4 text-center" key={d.name}>
-              <Link to={`./${d.name}`}>
-                <img src={d.src} alt={d.name}></img>
+              <img src={d.src} alt={d.name}></img>
+              <Link className="underline" to={`./${d.name}`}>
                 <h3>{d.name}</h3>
               </Link>
             </div>
